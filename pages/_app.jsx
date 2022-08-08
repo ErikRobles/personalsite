ad6c54e80f9b46d835da6e205644bd28c6fcea61
ErikRobles <erikjames69@hotmail.com>
2022-08-08 16:55:28 -0500

    cleaned up unused files

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
-// import type { AppProps } from 'next/app';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthContextProvider } from '../context/AuthContext';
